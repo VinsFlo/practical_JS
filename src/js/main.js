215cb1f5995cf60cc300c04da2159141aecaa57f
VinsFlo <vinsflorineus@gmail.com>
2021-09-09 18:27:12 +0300

    creating a timer

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,11 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 let modalState = {};
+let deadline = '2021-09-27';
+
 
 changeModalState(modalState);
 window.addEventListener('DOMContentLoaded', () => {
@@ -15,4 +18,5 @@ window.addEventListener('DOMContentLoaded', () => {
 	tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 	tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
 	forms(modalState);
+	timer('.container1', deadline);
 });
